d7683a99bf916664c91eae904f0cbab4f0684066
Donat Zubčić <donat.zubcic@gmail.com>
2025-04-07 20:05:15 +0200

    Remove .env and .history files

--- .history/models/table_20250316115929.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const teamSchema = new mongoose.Schema({
-  teamName: { type: String, required: true },
-  pointsPerTournament: [{ type: Number, default: 0 }], // Svaki element niza predstavlja bodove po turniru
-});
-
-module.exports = mongoose.model("Team", teamSchema);
